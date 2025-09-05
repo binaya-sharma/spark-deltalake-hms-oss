fcfab977684fa15c77994350dc32cd48cbd24894
BinayS <mebinayasharma@gmail.com>
2025-09-05 15:25:26 +0545

    delta path added, main.py changed

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from pyspark.sql import SparkSession
 from delta import configure_spark_with_delta_pip
 from pyspark.sql.functions import col, lit
+from pyspark.sql.types import *
 
-WAREHOUSE_DIR = "/warehouse"          # mounted local path for managed tables
-DELTA_PATH = "/warehouse/events"  # a specific Delta table path
+WAREHOUSE_DIR = "/warehouse"         # mounted local path for managed tables
+CUST_DELTA_PATH = "/warehouse/events/cust"  #different transformation have different path to preserve schema and data
 
 builder = (
     SparkSession.builder
@@ -11,49 +12,63 @@ builder = (
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
     .config("spark.sql.warehouse.dir", WAREHOUSE_DIR)
-    .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.1.0")  # ← add this
+    .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.1.0")
     .config("spark.ui.port", "4040")
     .config("spark.sql.shuffle.partitions", "1")
 )
 
 spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
-# 1) Create a small DataFrame
-df = spark.createDataFrame(
-    [
-        (1, "signup", "2025-09-05"),
-        (2, "click",  "2025-09-05"),
-        (3, "purchase", "2025-09-05"),
-        (4, "refund", "2025-09-05") 
-    ], #data
-    ["user_id", "event", "event_date"] #schema
-)
+# Define the schema for the customer sales data using structured type and fields.
+df_schema = StructType([
+    StructField("sale_id", IntegerType(), True),
+    StructField("customer_id", StringType(), True),
+    StructField("customer_name", StringType(), True),
+    StructField("gender", StringType(), True),
+    StructField("age", IntegerType(), True),
+    StructField("email", StringType(), True),
+    StructField("phone_number", StringType(), True),
+    StructField("city", StringType(), True),
+    StructField("loyalty_tier", StringType(), True),
+    StructField("store_name", StringType(), True),
+    StructField("product_name", StringType(), True),
+    StructField("supplier_name", StringType(), True),
+    StructField("employee_name", StringType(), True),
+    StructField("quantity", IntegerType(), True),
+    StructField("unit_price", DoubleType(), True),
+    StructField("total_sales", DoubleType(), True),
+    StructField("coupon_code", StringType(), True),
+    StructField("discount_percent", IntegerType(), True),
+    StructField("return_policy", StringType(), True),
+    StructField("warranty", StringType(), True),
+    StructField("customer_frequency", StringType(), True),
+    StructField("mode", StringType(), True),
+    StructField("sale_date", StringType(), True),  
+    StructField("sale_time", StringType(), True) 
+])
 
-#df.write.format("delta").mode("overwrite").save(DELTA_PATH)
+# Read the customer sales data from a CSV file into a DataFrame using the defined schema.
+df = (spark.read
+    .option("header", "false")  # no header in the file if header is present, change to true
+    .option("quote", '"')       # handle quoted values 
+    .option("delimiter", ",")  # comma-separated values
+    .option("inferSchema", "false") # we are providing schema, so no need to infer
+    .schema(df_schema)
+    .csv("/data/raw_data/cust-sample-data.txt") # path to the CSV file
+    )
 
-#spark.read.format("delta").load(DELTA_PATH).show(truncate=False)
+# simple transformation: filter rows where city is Kathmandu and add a new column ingest_date with a constant value
+df_city_ktm = df.filter(col("city") == "Kathmandu").withColumn("ingest_date", lit("2025-09-05")) 
 
-'''
-df = spark.createDataFrame(
-    [
-        (4, "refund-append", "2025-09-05") 
-    ], #data
-    ["user_id", "event", "event_date"] #schema
-)
-df.write.format("delta").mode("append").save(DELTA_PATH)
-'''
+# write the transformed data to a Delta table in overwrite mode 
+#df_city_ktm.write.format("delta").mode("overwrite").save(CUST_DELTA_PATH)
 
-spark.read.format("delta").load(DELTA_PATH).show(truncate=False)
+# read back the data from the Delta table and show the results
+df_ktm_read = spark.read.format("delta").load(CUST_DELTA_PATH)
+df_ktm_read.show()
 
-
-
-
-
-
-
-
-
-
+# print the schema of the read DataFrame
+df_ktm_read.printSchema()
 
 
 
@@ -62,5 +77,4 @@ spark.read.format("delta").load(DELTA_PATH).show(truncate=False)
 
 
 input("Press Enter to exit and stop Spark...")
-
 spark.stop()
---
+++ test_dir/2.py
@@ -0,0 +1,69 @@
+from pyspark.sql import SparkSession
+from delta import configure_spark_with_delta_pip
+from pyspark.sql.functions import col, lit
+
+WAREHOUSE_DIR = "/warehouse"          # mounted local path for managed tables
+DELTA_PATH = "/warehouse/events"  # a specific Delta table path
+
+builder = (
+    SparkSession.builder
+    .appName("spark-delta-local")
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    .config("spark.sql.warehouse.dir", WAREHOUSE_DIR)
+    .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.1.0")  # ← add this
+    .config("spark.ui.port", "4040")
+    .config("spark.sql.shuffle.partitions", "1")
+)
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+# 1) Create a small DataFrame
+df = spark.createDataFrame(
+    [
+        (1, "signup", "2025-09-05"),
+        (2, "click",  "2025-09-05"),
+        (3, "purchase", "2025-09-05"),
+        (4, "refund", "2025-09-05") 
+    ], #data
+    ["user_id", "event", "event_date"] #schema
+)
+
+#df.write.format("delta").mode("overwrite").save(DELTA_PATH)
+
+#spark.read.format("delta").load(DELTA_PATH).show(truncate=False)
+
+'''
+df = spark.createDataFrame(
+    [
+        (4, "refund-append", "2025-09-05") 
+    ], #data
+    ["user_id", "event", "event_date"] #schema
+)
+df.write.format("delta").mode("append").save(DELTA_PATH)
+'''
+
+spark.read.format("delta").load(DELTA_PATH).show(truncate=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+input("Press Enter to exit and stop Spark...")
+
+spark.stop()
